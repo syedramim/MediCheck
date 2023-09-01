9d7299b5dcf343005fa1a8fa4836a1cfff7fb8bb
Kaneis Zontanos <ramim@bc.edu>
2023-09-01 08:37:20 -0400

    Initial Commit

---
+++ MediCheck/MediCheckApp.swift
@@ -0,0 +1,17 @@
+//
+//  MediCheckApp.swift
+//  MediCheck
+//
+//  Created by Kaneis Zontanos on 9/1/23.
+//
+
+import SwiftUI
+
+@main
+struct MediCheckApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
